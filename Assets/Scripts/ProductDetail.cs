cae1440dd6e6371a7b7fe996fda98e876a5a5b32
Somnath Chowdhury <somnath.chowdhury86@gmail.com>
2022-11-12 16:28:20 +0530

    12/11/2022 - Import Model and Light Setup

---
+++ Assets/Scripts/ProductDetail.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductDetail : MonoBehaviour
+{
+    public int price;
+    public string quantity;
+    public string productColor;
+    public string pruductname;
+    public string pruductdescription;
+}
